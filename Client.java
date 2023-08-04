684e215e4e2681793ca0618b20a080e72e4ff34b
Corneille <cgbeaga@gmail.com>
2023-08-04 13:41:13 +0100

    Création de la catégorie client sous forme d'énumération et ajout des propriétés de la classe Client

---
+++ Client.java
@@ -0,0 +1,54 @@
+public class Client {
+    // propriété
+    long id;
+    String nomPrenoms;
+    String telephone;
+    CategorieClient categorieClient;
+
+    // Constructeur sans paramètre
+    public Client() {
+
+    }
+
+    // constructeur avec paramètre
+    public Client(long id, String nomPrenoms, String telephone, CategorieClient categorieClient) {
+        this.id = id;
+        this.nomPrenoms = nomPrenoms;
+        this.telephone = telephone;
+        this.categorieClient = categorieClient;
+    }
+
+    // Les accesseurs et les mutateurs
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNomPrenoms() {
+        return this.nomPrenoms;
+    }
+
+    public void setNomPrenoms(String nomPrenoms) {
+        this.nomPrenoms = nomPrenoms;
+    }
+
+    public String getTelephone() {
+        return this.telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public CategorieClient getCategorieClient() {
+        return this.categorieClient;
+    }
+
+    public void setCategorieClient(CategorieClient categorieClient) {
+        this.categorieClient = categorieClient;
+    }
+
+}
